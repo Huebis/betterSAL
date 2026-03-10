7154305c452cfd0bbef9ca827164416777cb0674
huebis <eliah.hueber@intergga.ch>
2026-03-10 15:42:55 +0100

    small bug fix

--- backend/main.py
+++ backend/main.py
@@ -511,7 +511,7 @@ def postFcmToken():
     print("alles hat funktioniert mit dem Token")
 
 
-    notification.sentNotificationToUserID(userID,"ANGEMOLDEN","Sie haben sich erfolgreich angemolden",5)
+    notification.sentNotificationToUserID(db,userID,"ANGEMOLDEN","Sie haben sich erfolgreich angemolden",5)
 
 
 
